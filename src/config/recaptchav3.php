a3a46e9ab62525384f502711016fdb24c75d3a60
Farman Khan <49769467+farmanafridi786@users.noreply.github.com>
2025-03-09 02:49:41 +0500

    Update recaptchav3.php
    add v2 support

--- src/config/recaptchav3.php
+++ src/config/recaptchav3.php
@@ -3,5 +3,7 @@ return [
     'origin' => env('RECAPTCHAV3_ORIGIN', 'https://www.google.com/recaptcha'),
     'sitekey' => env('RECAPTCHAV3_SITEKEY', ''),
     'secret' => env('RECAPTCHAV3_SECRET', ''),
+    'sitekeyv2' => env('RECAPTCHAV2_SITEKEY', ''),
+    'secretv2' => env('RECAPTCHAV2_SECRET', ''),
     'locale' => env('RECAPTCHAV3_LOCALE', '')
 ];
